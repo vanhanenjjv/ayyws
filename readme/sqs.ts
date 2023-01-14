b9babcfc52c9aebba38c41c26fbbf6de946dce95
Janne Vanhanen <janne.vanhanen@outlook.com>
2023-01-14 23:59:24 +0200

    Update sqs doc

--- readme/sqs.ts
+++ readme/sqs.ts
@@ -11,25 +11,28 @@ type Result =
   | { retry: true }
   | { retry: false }
 
-/* When . */
+/* Creates a result value that signals the SQSHandler 
+   wrapper function to retry record. */
 declare function retry(): Result
 
-/* Signal to not be retried. */
+/* Creates a result value that signals the SQSHandler 
+   wrapper function to not retry record. */
 declare function pass(): Result
 
 /* Function with similar interface to SQSHandler with two notable differences:
-   1. It is applied to SQSRecord instead of SQSEvent.
-   2. It expects a Result object in response.
+   1. It is applied to a record instead of the event.
+   2. It expects a Result object in return.
 */
 type HandlerFunction = (
-  /* Record that is being processed. */
+  /* Record that is being processed. Passed by the SQSHandler wrapper. */
   record: SQSRecord,
-  /* Passed directly from SQSHandler. */
+  /* Passed directly by the SQSHandler wrapper. */
   context: Context,
-  /* Passed directly from SQSHandler. */
+  /* Passed directly by the SQSHandler wrapper. */
   callback: Callback<void | SQSBatchResponse>
-  /* Signals the handler. */
+  /* Used by the SQSHandler wrapper to keep track of the records to be retried. */
 ) => Promise<Result> | Result
 
-/* Transforms given HandlerFunction into SQSHandler. */
+/* Creates a SQSHandler wrapper function which applies the given
+   HandlerFunction individually to each record. */
 declare function makeHandler(fn: HandlerFunction): SQSHandler
